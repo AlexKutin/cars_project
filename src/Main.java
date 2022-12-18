454a52746cd9b67580d125603a04965431076c96
Alexey Kutin <kutinalexeydm@gmail.com>
2022-12-18 14:05:45 +0300

    Homework 2 (Cars project) all parts

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,29 @@
 import transport.Car;
 import transport.Car.TyresType;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         Car ladaGranta = new Car("Lada", "Granta", 1.7, "желтый", 2015, "Россия",
-                "Manual", "Хэтчбек", "ABC0987654321", 5, null);
-        Car audiA8 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
+                "Manual", "Хэтчбек", "М670КУ199", 5, null);
+        Car audiA8 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия",
+                "Auto Transmission", "Sedan", "X089MK199", 5, TyresType.SUMMER,
+                new Car.Key(true, true));
         Car bmwZ8 = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
         Car kiaSportage = new Car("Kia", "Sportage 4 поколение", 2.4, "красный", 2018, "Южная Корея");
         Car hyundaiAvante = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
         Car kiaSorento = new Car("Kia", "Sorento", 2.5, " ", -600, "Южная Корея",
-                "Auto Transmission", "Универсал", "XJV1234567890", 7, TyresType.WINTER);
+                "Auto Transmission", "Универсал", "е001КХ199", 7,
+                TyresType.WINTER, new Car.Key(true, false));
+
+        ladaGranta.setInsurance(new Car.Insurance(LocalDate.now().plusDays(30), 4000, "765889"));
 
         System.out.println(ladaGranta);
         System.out.println(audiA8);
+        System.out.println(bmwZ8);
+        System.out.println("Change registration number...");
+        bmwZ8.setRegistrationNumber("Л593МК199");
         System.out.println(bmwZ8);
         System.out.println(kiaSportage);
         System.out.println(hyundaiAvante);
@@ -21,6 +31,9 @@ public class Main {
 
         System.out.println("Устанавливаем летние шины...");
         kiaSorento.changeTyresTypeBySeason(6);
+        System.out.println(kiaSorento);
+        System.out.println("Buy insurance...");
+        kiaSorento.setInsurance(new Car.Insurance(LocalDate.now().plusMonths(10), 10_000, "XY1234567"));
         System.out.println(kiaSorento);
     }
 }
--- src/transport/Car.java
+++ src/transport/Car.java
@@ -1,6 +1,12 @@
 package transport;
 
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.regex.Pattern;
+
 public class Car {
+    private static final Pattern REG_NUMBER_PATTERN = Pattern.compile("[а-яА-Я]\\d{3}[а-яА-Я]{2}\\d{3}");  // х000хх000
+
     public enum TyresType { SUMMER ("Летние"), WINTER ("Зимние");
         private final String tyresName;
 
@@ -11,6 +17,96 @@ public class Car {
         @Override
         public String toString() {
             return tyresName;
+        }
+    }
+
+    public static class Key {
+        private final boolean isRemoteEngineStart;
+        private final boolean isKeylessAccess;
+
+        public Key(boolean isRemoteEngineStart, boolean isKeylessAccess) {
+            this.isRemoteEngineStart = isRemoteEngineStart;
+            this.isKeylessAccess = isKeylessAccess;
+        }
+
+        public boolean isRemoteEngineStart() {
+            return isRemoteEngineStart;
+        }
+
+        public boolean isKeylessAccess() {
+            return isKeylessAccess;
+        }
+
+        @Override
+        public String toString() {
+            return "Удаленный запуск двигателя: " + booleanParamToString(isRemoteEngineStart) +
+                    ", Бесключевой доступ: " + booleanParamToString(isKeylessAccess);
+        }
+
+        private String booleanParamToString(boolean param) {
+            return param ? "есть" : "нет";
+        }
+    }
+
+    public static class Insurance {
+        private static final int INSURANCE_NUMBER_VALID_LENGTH = 9;
+        private static final String DEFAULT_INSURANCE_NUMBER = "XXX777999";
+        private final LocalDate insuranceValidityPeriod;
+        private final int insuranceCost;
+        private final String insuranceNumber;
+
+        public Insurance(LocalDate insuranceValidityPeriod, int insuranceCost, String insuranceNumber) {
+            if (insuranceValidityPeriod == null || insuranceValidityPeriod.isBefore(LocalDate.now())) {
+                this.insuranceValidityPeriod = LocalDate.now().plusYears(1);
+            } else {
+                this.insuranceValidityPeriod = insuranceValidityPeriod;
+            }
+            this.insuranceCost = Math.max(1, insuranceCost);
+            this.insuranceNumber = Objects.requireNonNullElse(insuranceNumber, DEFAULT_INSURANCE_NUMBER);
+        }
+
+        public LocalDate getInsuranceValidityPeriod() {
+            return insuranceValidityPeriod;
+        }
+
+        public int getInsuranceCost() {
+            return insuranceCost;
+        }
+
+        public String getInsuranceNumber() {
+            return insuranceNumber;
+        }
+
+        public void checkInsurancePeriod() {
+            if (isInsurancePeriodValid(insuranceValidityPeriod)) {
+                System.out.println("Страховка просрочена! Нужно срочно ехать оформлять новую страховку");
+            }
+        }
+
+        public String checkInsuranceNumber() {
+            if (isInsuranceNumberValid(insuranceNumber)) {
+                return "Ок";
+            } else {
+                return "Номер страховки некорректный!";
+            }
+        }
+
+        @Override
+        public String toString() {
+            if (isInsurancePeriodValid(insuranceValidityPeriod)) {
+                return String.format("Страховка {действует до: %s, стоимость: %d, номер страховки: %s (%s)}",
+                        insuranceValidityPeriod, insuranceCost, insuranceNumber, checkInsuranceNumber());
+            } else {
+                return "Страховка просрочена!";
+            }
+        }
+
+        private boolean isInsuranceNumberValid(String insuranceNumber) {
+            return insuranceNumber.length() == INSURANCE_NUMBER_VALID_LENGTH;
+        }
+
+        private boolean isInsurancePeriodValid(LocalDate insuranceDate) {
+            return LocalDate.now().compareTo(insuranceValidityPeriod) < 0;
         }
     }
 
@@ -25,6 +121,8 @@ public class Car {
     private String registrationNumber;  // Регистрационный номер»
     private final int seatsNumber;  // Количество мест
     private TyresType tyresType;    // тип резины «Летняя» или «Зимняя»
+    private final Key key;
+    private Insurance insurance;
 
     public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
         this(brand, model, engineVolume, color, productionYear, productionCountry, "", "Sedan", "", 4, TyresType.SUMMER);
@@ -32,6 +130,12 @@ public class Car {
 
     public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry,
                String gearBox, String bodyType, String registrationNumber, int seatsNumber, TyresType tyresType) {
+        this(brand, model, engineVolume, color, productionYear, productionCountry, gearBox, bodyType, registrationNumber,
+                seatsNumber, tyresType, createDefaultKey());
+    }
+
+    public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry,
+               String gearBox, String bodyType, String registrationNumber, int seatsNumber, TyresType tyresType, Key key) {
         this.brand = checkIsNotEmptyAndFill(brand);
         this.model = checkIsNotEmptyAndFill(model);
         this.engineVolume = checkAndFillEngineVolume(engineVolume);
@@ -44,13 +148,15 @@ public class Car {
         this.bodyType = checkBodyTypeAndFill(bodyType);
         this.registrationNumber = checkIsNotEmptyAndFill(registrationNumber);
         this.tyresType = checkTyresTypeAndFill(tyresType);
+        this.key = checkCarKeyAndFill(key);
     }
 
     @Override
     public String toString() {
-        return String.format("%s %s, %d год выпуска, страна сборки: %s, цвет кузова: %s, объем двигателя: %.1f литра\n" +
-                        "    КПП: %s, количество место: %d, тип кузова: %s, рег. номер: %s, тип шин: %s",
-                brand, model, productionYear, productionCountry, color, engineVolume, gearBox, seatsNumber, bodyType, registrationNumber, tyresType);
+        return String.format("%s %s, %d год выпуска, страна сборки: %s, цвет кузова: %s, объем двигателя: %.1f литра, " +
+                        "КПП: %s,\n количество мест: %d, тип кузова: %s, рег. номер: %s (%s), тип шин: %s, %s, %s",
+                brand, model, productionYear, productionCountry, color, engineVolume, gearBox, seatsNumber,
+                bodyType, registrationNumber, regNumberValidResult(), tyresType, key, insurance == null ? "страховка отсутствует" : insurance);
     }
 
     public String getBrand() {
@@ -117,6 +223,26 @@ public class Car {
         this.tyresType = tyresType;
     }
 
+    public Key getKey() {
+        return key;
+    }
+
+    public Insurance getInsurance() {
+        return insurance;
+    }
+
+    public void setInsurance(Insurance insurance) {
+        this.insurance = insurance;
+    }
+
+    public boolean isRegistrationNumberValid(String regNumber) {
+        return REG_NUMBER_PATTERN.matcher(regNumber).matches();
+    }
+
+    private String regNumberValidResult() {
+        return isRegistrationNumberValid(registrationNumber) ? "Ok" : "Error";
+    }
+
     public void changeTyresTypeBySeason(int month) {
         if (month < 1 || month > 12) {
             throw new IllegalArgumentException("The month number should be from 1 to 12: " + month);
@@ -180,5 +306,16 @@ public class Car {
 
     private int checkSeatsNumberAndFill(int seatsNumber) {
         return Math.max(seatsNumber, 2);
+    }
+
+    private Key checkCarKeyAndFill(Key key) {
+        if (key == null) {
+            return createDefaultKey();
+        }
+        return key;
+    }
+
+    private static Car.Key createDefaultKey() {
+        return new Key(false, false);
     }
 }
