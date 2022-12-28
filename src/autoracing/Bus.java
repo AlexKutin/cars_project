c2217b21bff48d83266d3e92b52e79c32309f93e
Alexey Kutin <kutinalexeydm@gmail.com>
2022-12-28 23:57:23 +0300

    Homework 2_5

--- src/autoracing/Bus.java
+++ src/autoracing/Bus.java
@@ -2,8 +2,56 @@ package autoracing;
 
 public class Bus<T extends DriverD> extends Transport<T> implements Competing<T> {
 
+    enum BusCapacity {
+        ESPECIALLY_SMALL(null, 10),
+        SMALL(null, 25),
+        AVERAGE(40, 50),
+        LARGE(60, 80),
+        ESPECIALLY_LARGE(100, 120);
+
+        private final Integer from;
+        private final Integer upTo;
+
+        BusCapacity(Integer from, Integer upTo) {
+            this.from = from;
+            this.upTo = upTo;
+        }
+
+        @Override
+        public String toString() {
+            if (from == null && upTo == null) {
+                return "Вместимость: не задана!";
+            } else if (from != null && upTo == null) {
+                return String.format("Вместимость: от %d мест", from);
+            } else if (from == null) {
+                return String.format("Вместимость: до %d мест", upTo);
+            } else {
+                return String.format("Вместимость: %d - %d мест", from, upTo);
+            }
+        }
+    }
+
+    private BusCapacity busCapacity;
+
+    public BusCapacity getBusCapacity() {
+        return busCapacity;
+    }
+
+    public void setBusCapacity(BusCapacity busCapacity) {
+        this.busCapacity = busCapacity;
+    }
+
     public Bus(String brand, String model, double engineVolume) {
         super(brand, model, engineVolume);
+    }
+
+    @Override
+    public void printType() {
+        if (busCapacity == null) {
+            System.out.println("Данных по транспортному средству недостаточно");
+        } else {
+            System.out.println(busCapacity);
+        }
     }
 
     @Override
--- src/autoracing/Truck.java
+++ src/autoracing/Truck.java
@@ -2,8 +2,54 @@ package autoracing;
 
 public class Truck<T extends DriverC> extends Transport<T> implements Competing<T> {
 
+    enum LoadCapacity {
+        N1(null, 3.5f),
+        N2(3.5f, 12f),
+        N3(12f, null);
+
+        private final Float from;
+        private final Float upTo;
+
+        LoadCapacity(Float from, Float upTo) {
+            this.from = from;
+            this.upTo = upTo;
+        }
+
+        @Override
+        public String toString() {
+            if (from == null && upTo == null) {
+                return "Грузоподъемность: не задана!";
+            } else if (from != null && upTo == null) {
+                return String.format("Грузоподъемность: от %.1f тонн", from);
+            } else if (from == null) {
+                return String.format("Грузоподъемность: до %.1f тонн", upTo);
+            } else {
+                return String.format("Грузоподъемность: от %.1f тонн до %.1f тонн", from, upTo);
+            }
+        }
+    }
+
+    private LoadCapacity loadCapacity;
+
+    public LoadCapacity getLoadCapacity() {
+        return loadCapacity;
+    }
+
+    public void setLoadCapacity(LoadCapacity loadCapacity) {
+        this.loadCapacity = loadCapacity;
+    }
+
     public Truck(String brand, String model, double engineVolume) {
         super(brand, model, engineVolume);
+    }
+
+    @Override
+    public void printType() {
+        if (loadCapacity == null) {
+            System.out.println("Данных по транспортному средству недостаточно");
+        } else {
+            System.out.println(loadCapacity);
+        }
     }
 
     @Override
